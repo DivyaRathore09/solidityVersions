d4aa2bd8e88aa93ee4e65928e919a552534963b4
DivyaRathore09 <37830852+DivyaRathore09@users.noreply.github.com>
2023-03-03 02:17:38 +0530

    Create solidity.rb

---
+++ solcv0_4_16/solidity.rb
@@ -0,0 +1,39 @@
+#------------------------------------------------------------------------------
+# solidity.rb
+#
+# Homebrew formula for solidity.  Homebrew (http://brew.sh/) is
+# the de-facto standard package manager for OS X, and this Ruby script
+# contains the metadata used to map command-line user settings used
+# with the 'brew' command onto build options.
+#
+# Our documentation for the solidity Homebrew setup is at:
+#
+# http://solidity.readthedocs.io/en/latest/installing-solidity.html
+#
+# (c) 2014-2017 solidity contributors.
+#------------------------------------------------------------------------------
+
+require 'formula'
+
+class Solidity < Formula
+
+  desc "The Solidity Contract-Oriented Programming Language"
+  homepage "http://solidity.readthedocs.org"
+  url "https://github.com/ethereum/solidity/releases/download/v0.4.16/solidity_0.4.16.tar.gz"
+  version "0.4.16"
+  sha256 "18320cde7e2c37e57ef501b437103497efb2e300d0dedc47e00038ce35819921"
+
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  depends_on "cryptopp"
+  depends_on "gmp"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/solc", "--version"
+  end
+end
