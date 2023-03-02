0213a06f69534031218a6df00754c56e1a27d734
DivyaRathore09 <37830852+DivyaRathore09@users.noreply.github.com>
2023-03-03 03:15:43 +0530

    Create solidity.rb

---
+++ solcv0_8_8/solidity.rb
@@ -0,0 +1,37 @@
+#------------------------------------------------------------------------------
+# solidity.rb
+#
+# Homebrew formula for solidity.  Homebrew (http://brew.sh/) is
+# the de-facto standard package manager for OS X, and this Ruby script
+# contains the metadata used to map command-line user settings used
+# with the 'brew' command onto build options.
+#
+# Our documentation for the solidity Homebrew setup is at:
+#
+# http://solidity.readthedocs.io/en/latest/installing-solidity.html
+#
+# (c) 2014-2017 solidity contributors.
+#------------------------------------------------------------------------------
+
+class Solidity < Formula
+  desc "The Solidity Contract-Oriented Programming Language"
+  homepage "https://docs.soliditylang.org"
+  url "https://github.com/ethereum/solidity/releases/download/v0.8.8/solidity_0.8.8.tar.gz"
+  version "0.8.8"
+  sha256 "d15827496f4baa47aceabfd2342370aad62a2042fa5d5e501b8138506b288e1d"
+
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  # Note: due to a homebrew limitation, ccache will always be detected and cannot be turned off.
+  depends_on "ccache" => :build
+  depends_on "z3"
+
+  def install
+    system "cmake", ".", *std_cmake_args, "-DTESTS=OFF"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/solc", "--version"
+  end
+end
