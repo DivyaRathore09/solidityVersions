c8efc54451fdb95993b618253c7815ee2e695c00
DivyaRathore09 <37830852+DivyaRathore09@users.noreply.github.com>
2023-03-03 00:50:47 +0530

    Create solidity.rb

---
+++ solcv0514/solidity.rb
@@ -0,0 +1,22 @@
+class Solidity < Formula
+  desc "The Solidity Contract-Oriented Programming Language"
+  homepage "http://solidity.readthedocs.org"
+  url "https://github.com/ethereum/solidity/releases/download/v0.5.14/solidity_0.5.14.tar.gz"
+  version "0.5.14"
+  sha256 "256ba216089f22813c63b41ac44fc699eb2a537c3ff18980e4e48832e13b35bb"
+
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  # Note: due to a homebrew limitation, ccache will always be detected and cannot be turned off.
+  depends_on "ccache" => :build
+  depends_on "z3"
+
+  def install
+    system "cmake", ".", *std_cmake_args, "-DTESTS=OFF"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/solc", "--version"
+  end
+end
