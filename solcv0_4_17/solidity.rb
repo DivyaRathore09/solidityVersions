1ef3ef35e9ebf72d22f267a2ab651b19ae137448
DivyaRathore09 <37830852+DivyaRathore09@users.noreply.github.com>
2023-03-03 02:07:43 +0530

    Create solidity.rb

---
+++ solcv0_4_17/solidity.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Solidity < Formula
+
+  desc "The Solidity Contract-Oriented Programming Language"
+  homepage "http://solidity.readthedocs.org"
+  url "https://github.com/ethereum/solidity/releases/download/v0.4.17/solidity_0.4.17.tar.gz"
+  version "0.4.17"
+  sha256 "934446983592a97d6e090130614ee1173e535d4b76e24ebce1857dd16ff514b1"
+
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  depends_on "cryptopp"
+  depends_on "gmp"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/solc", "--version"
+  end
+end
